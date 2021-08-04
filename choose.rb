e3385b762ce4607fb70f65aec11771b7f2a244fd
eatplaynap <eatplaynap329@gmail.com>
2021-08-04 15:49:01 +0900

    コマンド実行者のいるボイスチャンネル名を取得して表示

--- choose.rb
+++ choose.rb
@@ -1,1 +1,14 @@
 # frozen_string_literal: true
+
+bot = Discordrb::Commands::CommandBot.new token: "#{ENV['TOKEN']}", prefix: '!'
+
+bot.command(:you) do |event|
+  channel = event.user.voice_channel
+  if channel.nil?
+    event.send_message "ボイスチャンネルに入ってからコマンドを実行してください"
+  else
+    event.send_message "#{channel.name}"
+  end
+end
+
+bot.run
