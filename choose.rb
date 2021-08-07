c778be966ae6720add86c879ffbeaad000029ebc
eatplaynap <eatplaynap329@gmail.com>
2021-08-07 14:38:50 +0900

    人を1人選ぶbot動作版

--- choose.rb
+++ choose.rb
@@ -2,12 +2,17 @@
 
 bot = Discordrb::Commands::CommandBot.new token: "#{ENV['TOKEN']}", prefix: '!'
 
-bot.command(:you) do |event|
+bot.command(:you)  do |event|
   channel = event.user.voice_channel
   if channel.nil?
     event.send_message "ボイスチャンネルに入ってからコマンドを実行してください"
+  elsif channel.users.all?{ |user| user.self_muted? }
+    event.send_message "ミュートを解除してください"
   else
-    event.send_message "#{channel.name}"
+    unmuted_users = channel.users.reject{ |user| user.self_muted? }
+    user_names = unmuted_users&.map(&:name)
+    chosen_user = user_names.sample
+    event.send_message chosen_user
   end
 end
 
