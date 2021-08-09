7dc40199d451f4a56401a4f0d5ad9772b201ddf4
eatplaynap <eatplaynap329@gmail.com>
2021-08-09 11:16:48 +0900

    rubocop OK

--- choose.rb
+++ choose.rb
@@ -3,16 +3,16 @@
 require 'dotenv/load'
 require 'discordrb'
 
-bot = Discordrb::Commands::CommandBot.new token: "#{ENV['TOKEN']}", prefix: '!'
+bot = Discordrb::Commands::CommandBot.new token: (ENV['TOKEN']).to_s, prefix: '!'
 
-bot.command(:you)  do |event|
+bot.command(:you) do |event|
   channel = event.user.voice_channel
   if channel.nil?
-    event.send_message "ボイスチャンネルに入ってからコマンドを実行してください"
-  elsif channel.users.all?{ |user| user.self_muted? }
-    event.send_message "ミュートを解除してください"
+    event.send_message 'ボイスチャンネルに入ってからコマンドを実行してください'
+  elsif channel.users.all?(&:self_muted?)
+    event.send_message 'ミュートを解除してください'
   else
-    unmuted_users = channel.users.reject{ |user| user.self_muted? }
+    unmuted_users = channel.users.reject(&:self_muted?)
     user_names = unmuted_users&.map(&:name)
     chosen_user = user_names.sample
     event.send_message chosen_user
