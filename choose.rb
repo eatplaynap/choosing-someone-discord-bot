8680a9da0e5fdf27cb768d41504771eaa648ae94
yana-gi <yanagisawa.hika@gmail.com>
2021-08-10 10:25:18 +0900

    人数を指定できるようにした

--- choose.rb
+++ choose.rb
@@ -5,17 +5,19 @@ require 'discordrb'
 
 bot = Discordrb::Commands::CommandBot.new token: (ENV['TOKEN']).to_s, prefix: '!'
 
-bot.command(:you) do |event|
+bot.command(:you) do |event, number|
   channel = event.user.voice_channel
   if channel.nil?
     event.send_message 'ボイスチャンネルに入ってからコマンドを実行してください'
   elsif channel.users.all?(&:self_muted?)
     event.send_message 'ミュートを解除してください'
   else
-    unmuted_users = channel.users.reject(&:self_muted?)
-    user_names = unmuted_users&.map(&:name)
-    chosen_user = user_names.sample
-    event.send_message chosen_user
+        number　= number&.to_i || 1
+        unmuted_users = channel.users.reject(&:self_muted?)
+        user_names = unmuted_users&.map(&:name)
+        chosen_users = user_names.sample.(number)
+        event.send_message chosen_users
+      end
   end
 end
 
