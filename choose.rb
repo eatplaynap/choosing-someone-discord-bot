2e1f7b73d5b841042672b807111096f2788dd8ca
yuichi tanaka <u1tnk1123@gmail.com>
2021-08-10 12:06:25 +0900

    行間のスペースを調整

--- choose.rb
+++ choose.rb
@@ -7,9 +7,7 @@ bot = Discordrb::Commands::CommandBot.new token: (ENV['TOKEN']).to_s, prefix: '!
 
 bot.command(:you) do |event, number|
   channel = event.user.voice_channel
-
   retutn event.send_message('ボイスチャンネルに入ってからコマンドを実行してください') if channel.nil?
-
   return event.send_message('ミュートを解除してください') if channel.users.all?(&:self_muted?)
 
   number = number&.to_i || 1
@@ -18,6 +16,7 @@ bot.command(:you) do |event, number|
   unmuted_users = channel.users.reject(&:self_muted?)
   user_names = unmuted_users.map(&:name)
   chosen_users = user_names.sample(number)
+
   event.send_message chosen_users.join(", ")
 end
 
