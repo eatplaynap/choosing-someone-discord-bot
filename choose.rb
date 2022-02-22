61c558907a77b641a7b02dc7cbe0fe0be3560571
eatplaynap <eatplaynap329@gmail.com>
2022-02-22 20:19:56 +0900

    Translate command messages into English

--- choose.rb
+++ choose.rb
@@ -11,17 +11,17 @@ bot = Discordrb::Commands::CommandBot.new(
 
 bot.command(:you) do |event, number|
   channel = event.user.voice_channel
-  return event.send_message('ボイスチャンネルに入ってからコマンドを実行してください') if channel.nil?
-  return event.send_message('ミュートを解除してください') if channel.users.all?(&:self_muted?)
+  return event.send_message('Join a voice channel before executing the command!') if channel.nil?
+  return event.send_message("Huh, seems like you're all muted!") if channel.users.all?(&:self_muted?)
 
   number = number&.to_i || 1
-  return event.send_message('選択人数は正の整数を入れてください') unless number.positive?
+  return event.send_message('The number should be greater than 0!') unless number.positive?
 
   unmuted_users = channel.users.reject(&:self_muted?)
   user_names = unmuted_users.map(&:name)
   chosen_users = user_names.sample(number)
 
-  event.send_message chosen_users.join(", ")
+  event.send_message chosen_users.join(', ')
 end
 
 bot.run
